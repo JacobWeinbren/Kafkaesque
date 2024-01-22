7d8d1a9d1bb71513c85234d312a59aa5612fd135
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 12:07:13 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
 	output: "server",
 	integrations: [tailwind()],
 	adapter: cloudflare(),
+	image: {
+		service: {
+			entrypoint: "@astrojs/cloudflare/image-service",
+		},
+	},
 });
