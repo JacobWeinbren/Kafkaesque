33490953683d5fb7bd961388b97987388a0066cb
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 02:04:10 +0000

    Switched to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: cloudflare({ mode: "directory" }),
+	adapter: vercelServerless(),
 });
