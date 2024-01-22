2bef1daa2cd3a204d8d4cd7541891a519e3857d8
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 14:24:54 +0000

    Added head component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,23 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
 	output: "server",
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind(), sitemap(), compress()],
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
 		},
 	}),
+	vite: {
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
+	},
 });
