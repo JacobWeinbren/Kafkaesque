a1cd71df79884d78aa1d05984baf7d9eb071bdab
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 12:20:20 +0000

    Set the font to inter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+			},
+			fontWeight: {
+				normal: 400,
+				medium: 500,
+				semibold: 600,
+				bold: 700,
+				light: 300,
+			},
+		},
 	},
 	plugins: [],
-}
+};
