4cccb5a7db5107bfe8bc975817ee6549971c6180
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 13:21:02 +0000

    Testing compression functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import compress from "astro-compress";
 
 export default defineConfig({
 	output: "server",
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		compress({
+			Image: false,
+		}),
+	],
 	adapter: vercel(),
 });
