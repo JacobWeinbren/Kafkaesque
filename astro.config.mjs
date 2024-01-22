8b0479330efca4d7be5e79108522c3a5521373dd
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 13:27:35 +0000

    Testing minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import compress from "astro-compress";
+import minify from "astro-min";
 
+// https://astro.build/config
 export default defineConfig({
 	output: "server",
-	integrations: [
-		tailwind(),
-		compress({
-			Image: false,
-		}),
-	],
+	integrations: [tailwind(), minify()],
 	adapter: vercel(),
 });
