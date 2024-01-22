ae93c83d5f9d267a2c21f99b3475ea16e927dfe1
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 03:02:13 +0000

    Downgraded sharp
    https://github.com/lovell/sharp/issues/3870

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import vercelServerless from "@astrojs/vercel/serverless";
+import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
 	adapter: vercelServerless(),
+	integrations: [Compress()],
 });
