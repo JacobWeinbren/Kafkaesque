0fcd8138099da6fe0e03bc07ca85c09af639b14a
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 13:12:00 +0000

    Compression with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
 	output: "server",
-	integrations: [tailwind()],
+	integrations: [tailwind(), compress()],
 	adapter: vercel(),
 });
