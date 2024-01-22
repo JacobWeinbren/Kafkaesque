ae9f7b5e157b14458303010c375c7909067ba047
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 02:59:41 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import vercelServerless from "@astrojs/vercel/serverless";
-import Compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
 	adapter: vercelServerless(),
-	integrations: [Compress()],
 });
