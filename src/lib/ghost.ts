c152adc2007ba9329b1f2053c207e20f7d594488
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 17:19:25 +0000

    more efficient param collection

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -6,20 +6,11 @@ const api = new TSGhostContentAPI(
 	"v5.47.0"
 );
 
-export async function getBlogPosts() {
-	const response = await api.posts
-		.browse({
-			limit: 10,
-		})
-		.fields({
-			title: true,
-			slug: true,
-			id: true,
-			reading_time: true,
-			feature_image: true,
-			html: true,
-		})
-		.fetch();
+export async function getBlogPosts(
+	fields = {},
+	limit?: number
+) {
+	const response = await api.posts.browse({ limit }).fields(fields).fetch();
 	if (!response.success) {
 		const errorResponse = response as {
 			success: false;
