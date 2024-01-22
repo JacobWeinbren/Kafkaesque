63b33105b62acffc5634ff626f81182c6ddc0aa4
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-22 16:49:50 +0000

    Switching to cloudflare, again

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,7 +1,29 @@
-import GhostContentAPI from "@tryghost/content-api";
+import { TSGhostContentAPI } from "@ts-ghost/content-api";
 
-export const ghostClient = new GhostContentAPI({
-	url: "https://kafkaesque.digitalpress.blog",
-	key: import.meta.env.CONTENT_API_KEY,
-	version: "v5.0",
-});
+const api = new TSGhostContentAPI(
+	"https://kafkaesque.digitalpress.blog" || "",
+	process.env.GHOST_CONTENT_API_KEY || "",
+	"v5.47.0"
+);
+
+export async function getBlogPosts() {
+	const response = await api.posts
+		.browse({
+			limit: 10,
+		})
+		.fields({
+			title: true,
+			slug: true,
+			id: true,
+		})
+		.fetch();
+	if (!response.success) {
+		const errorResponse = response as {
+			success: false;
+			errors: { message: string; type: string }[];
+		};
+		console.error(errorResponse.errors.map((e) => e.message).join(", "));
+		return [];
+	}
+	return response.data;
+}
