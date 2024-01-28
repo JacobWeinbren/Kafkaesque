aacb1336649d214c08973f9f1e8714d70bbb634b
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-28 09:07:15 +0000

    Improved search efficency

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,24 +1,25 @@
 import { getBlogPosts } from "@/lib/ghost";
 import Fuse from "fuse.js";
 
-export const GET = async ({ params, request }) => {
+const allPosts = await getBlogPosts({
+	title: true,
+	slug: true,
+	excerpt: true,
+	published_at: true,
+	feature_image: true,
+});
+
+const fuse = new Fuse(allPosts, {
+	keys: ["title", "html"],
+	threshold: 0.4,
+});
+
+export const GET = async ({ request }) => {
 	const url = new URL(request.url);
 	const query = url.searchParams.get("query");
-	console.log(query);
-	const allPosts = await getBlogPosts({
-		title: true,
-		slug: true,
-		excerpt: true,
-		published_at: true,
-		feature_image: true,
-	});
-
-	const fuse = new Fuse(allPosts, {
-		keys: ["title", "html"],
-		threshold: 0.4,
-	});
 
 	const results = fuse.search(query).map((result) => result.item);
+
 	return new Response(JSON.stringify(results), {
 		headers: {
 			"Content-Type": "application/json",
