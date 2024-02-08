9212a855145d3b3e09fdb304394e08dfa1702735
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-08 22:15:11 +0000

    Switched to vercel properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
 	output: "server",
-	integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		react(),
+	],
 	vite: {
 		resolve: {
 			alias: {
