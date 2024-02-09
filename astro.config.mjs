df2e5bd650f8f473522b4a054a1c97db4a99b8c7
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-09 21:51:19 +0000

    Added back package lock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
-/*
-export default defineConfig({
-	site: "https://kafkaesque.blog/",
-	output: "server",
-	integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
-	vite: {
-		resolve: {
-			alias: {
-				"@": "/src",
-			},
-		},
-	},
-	redirects: {
-		"/blog": "/blog/1",
-	},
-	adapter: cloudflare({ imageService: "cloudflare" }),
-});
-*/
 
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
