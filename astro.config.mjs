5e20b8ae7cf4f3177270ebc8578e71292eab8889
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-09 21:27:34 +0000

    Switching back to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
 	output: "server",
-	integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		react(),
+	],
 	vite: {
 		resolve: {
 			alias: {
@@ -16,16 +22,12 @@ export default defineConfig({
 			},
 		},
 	},
-	image:
-		process.env.NODE_ENV === "production"
-			? {
-					service: {
-						entrypoint: "@astrojs/cloudflare/image-service",
-					},
-			  }
-			: {},
 	redirects: {
 		"/blog": "/blog/1",
 	},
-	adapter: cloudflare(),
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
