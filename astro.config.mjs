1e8ed4be1108bb764c6a59a5e3c66ec20f34d7e9
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-09 21:30:39 +0000

    For cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
 	output: "server",
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		react(),
-	],
+	integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
 	vite: {
 		resolve: {
 			alias: {
@@ -25,9 +19,5 @@ export default defineConfig({
 	redirects: {
 		"/blog": "/blog/1",
 	},
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
+	adapter: cloudflare({ imageService: "cloudflare" }),
 });
