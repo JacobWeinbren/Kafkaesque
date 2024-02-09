056edb3fbf4a76a25663022b880d5881989d8469
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-09 21:34:02 +0000

    For vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+/*
 export default defineConfig({
 	site: "https://kafkaesque.blog/",
 	output: "server",
@@ -20,4 +21,28 @@ export default defineConfig({
 		"/blog": "/blog/1",
 	},
 	adapter: cloudflare({ imageService: "cloudflare" }),
+});
+*/
+
+export default defineConfig({
+	site: "https://kafkaesque.blog/",
+	output: "server",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		react(),
+	],
+	vite: {
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
+	},
+	redirects: {
+		"/blog": "/blog/1",
+	},
+	adapter: vercel(),
 });
