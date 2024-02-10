96da12c75256f99e4f96ae9477ad27f4182c4537
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-10 19:41:51 +0000

    For the galaxy fold

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,15 @@ module.exports = {
 	],
 	prefix: "",
 	theme: {
-		fluid: {},
+		screens: {
+			xxs: "320px",
+			xs: "480px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
 		container: {
 			center: true,
 			padding: "2rem",
