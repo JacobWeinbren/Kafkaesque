f861a573a98a841547573da0f3cb65b3b5be8389
Jacob Weinbren <jacobweinbren@gmail.com>
2024-04-02 00:48:17 +0100

    Added sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+	site: "https://kafkaesque.blog/",
 	output: "server",
 	integrations: [
 		tailwind({
