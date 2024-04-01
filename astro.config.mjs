ac84b3b5e8dc35e18b655b4d3a206c481f7ae7ce
Jacob Weinbren <jacobweinbren@gmail.com>
2024-04-02 00:43:39 +0100

    removed default url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-	site: "https://kafkaesque.blog/",
 	output: "server",
 	integrations: [
 		tailwind({
