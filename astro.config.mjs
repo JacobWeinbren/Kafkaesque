38a6e8ed249d1f8b12048dba1fd95cb8021d5810
Jacob Weinbren <jacobweinbren@gmail.com>
2025-02-19 23:59:40 +0000

    Adds analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,42 +9,44 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-    site: "https://kafkaesque.blog",
-    integrations: [
-      tailwind({ config: { applyBaseStyles: false } }),
-      react(),
-      compress({ css: true, html: true, img: true, js: true, svg: true }),
-      sitemap(),
-      svelte(),
-    ],
-    output: "server",
-    adapter: vercel({
-        analytics: true,
-        imageService: true,
-        maxDuration: 60,
-        edgeMiddleware: true,
-    }),
-    image: {
-        domains: ["cdn.hashnode.com"],
-        remotePatterns: [{ protocol: "https", hostname: "cdn.hashnode.com" }],
-        format: ["avif", "webp"],
-        serviceEntryPoint: "@astrojs/image/sharp",
-    },
-    prefetch: { prefetchAll: true },
-    vite: {
-        build: {
-            cssMinify: "lightningcss",
-            minify: true,
-            rollupOptions: {
-                output: {
-                    manualChunks: {
-                        "react-vendor": ["react", "react-dom"],
-                        search: ["fuse.js", "lodash/debounce"],
-                    },
-                },
-            },
-        },
-        ssr: { noExternal: ["@heroicons/react"] },
-        resolve: { alias: { "@": "/src" } },
-    },
+	site: "https://kafkaesque.blog",
+	integrations: [
+		tailwind({ config: { applyBaseStyles: false } }),
+		react(),
+		compress({ css: true, html: true, img: true, js: true, svg: true }),
+		sitemap(),
+		svelte(),
+	],
+	output: "server",
+	adapter: vercel({
+		imageService: true,
+		maxDuration: 60,
+		edgeMiddleware: true,
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	image: {
+		domains: ["cdn.hashnode.com"],
+		remotePatterns: [{ protocol: "https", hostname: "cdn.hashnode.com" }],
+		format: ["avif", "webp"],
+		serviceEntryPoint: "@astrojs/image/sharp",
+	},
+	prefetch: { prefetchAll: true },
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+			minify: true,
+			rollupOptions: {
+				output: {
+					manualChunks: {
+						"react-vendor": ["react", "react-dom"],
+						search: ["fuse.js", "lodash/debounce"],
+					},
+				},
+			},
+		},
+		ssr: { noExternal: ["@heroicons/react"] },
+		resolve: { alias: { "@": "/src" } },
+	},
 });
